afcf912fc2089e279b337dd3d88d95bf0d251d90
Kevin Huria <huriakevin@gmail.com>
2022-10-01 13:39:28 +0300

    Added changes to app.py

--- FirstFlaskApp/app.py
+++ FirstFlaskApp/app.py
@@ -1,4 +1,22 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,render_template
+import psycopg2
+
+app=Flask(__name__)
+def get_db_connection():
+    connection = psycopg2.connect(
+        host="localhost", database="test", port="5432", user="postgres", password="Empire*2")
+    return connection
+
+@app.route('/')
+def index():
+    connection=get_db_connection()
+    cursor=connection.cursor()
+    cursor.execute('SELECT * FROM books;')
+    books=cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return render_template('index.html',books=books)
+
+
 
 
